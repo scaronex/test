7b8d92f9ea825db24aeb532fb721aba7be0a35a9
wojtek <scarone10@gmail.com>
2019-04-07 21:22:13 +0200

    Gotowy raport .Zmiana wyglądu . Rozliczenie salda

--- raport2.php
+++ raport2.php
@@ -7,9 +7,6 @@
   <link rel="stylesheet" type="text/css" href="css/rap.css">
 </head>
 <body>
-
-
-
 <?php
 $day1 = $_POST['Data'];
 $day2 = $_POST['Data1'];
@@ -24,8 +21,8 @@ echo<<<END
           <tr>
             <th> DATA ZADANIA </th>
             <th> RODZAJ </th>
-            <th>  KP  </th>
-            <th>  KW  </th>
+            <th>WPŁATA  KP  </th>
+            <th>WYPŁATA  KW  </th>
             <th>ZADANIE</th>
             <th>SALDO</th>
           </tr>
@@ -36,23 +33,28 @@ END;
 while($row = mysqli_fetch_assoc($result))
 {
 
-      if ($row['fl_rodzaj']!=1) {
-      $kpczykw="KW";
-      } else
+      if ($row['fl_rodzaj']!=1)
       {
-      $kpczykw="KP";
+        $kpczykw="KW";
+        $kw=$row['kwota'];
+        $kp=0;
       }
-      $idfetch=$row['jakie_idd'];
+       else
+      {
+        $kpczykw="KP";
+        $kp=$row['kwota'];
+        $kw=0;
+      }
+        $idfetch=$row['jakie_idd'];
+        $dbs = mysqli_connect($host, $login,$pass, $dbname) or die("Błąd połączenia3 !") ;
+               mysqli_set_charset($db,"utf8");
+        $qs = "SELECT sl.opis  FROM koszty as ko
+              join slowniki as sl on ko.rodzaj_koszt = sl.id
+              where ko.id=$idfetch";
+        $rodzaj = mysqli_query($dbs, $qs)or die("błąd połączenia4 ") ;
+        $rodzajkosztu=mysqli_fetch_assoc($rodzaj) ;
 
-      $db = mysqli_connect($host, $login,$pass, $dbname) or die("Błąd połączenia2 !") ;
-       mysqli_set_charset($db,"utf8");
-      $q = "SELECT sl.opis  FROM koszty as ko
-            join slowniki as sl on ko.rodzaj_koszt = sl.id
-            where ko.id=$idfetch";
-      $rodzaj = mysqli_query($db, $q)or die("błąd połączenia1 ") ;
-      $rodzajkosztu=$rodzaj;
-
-echo "<tr><td>".$row["add_date"]."</td><td>".$rodzajkosztu."<td>".$row['kwota']."<td>".$row['kwota'].
+echo "<tr><td>".$row["add_date"]."</td><td>".$rodzajkosztu['opis']."<td>".$kp."<td>".$kw.
 "<td>".$row['kwota']."<td>".$row['zliczsaldo']."<td>".$kpczykw."</td></tr>";
 }
 echo<<<END
